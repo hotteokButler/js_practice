2919aaf57bc3a522743ee8b9a800175042fb8bd0
hotteokButler <ygs12567@gmail.com>
2024-08-29 12:40:45 +0900

    중복값 확인 문제 해답 관련

--- 2_ProblemSolving/코드연습/Frequency_counter02.js
+++ 2_ProblemSolving/코드연습/Frequency_counter02.js
@@ -13,7 +13,7 @@ areThereDuplicates('a','b','c','a') //true
 
 // 다중 포인터로 풀어보려함
 // 1. 가변적으로 인자를 받아야 하기 때문에 spread 연산자로 배열로 받음
-// 2. 아무런 인자도 받지 않거나, 비교 할 수 없이 1개의 인자만 받게되면 false 리턴 아니라면 정렬
+// 2. 아무런 인자도 받지 않거나, 비교 할 수 없이 1개의 인자만 받게되면 false 리턴 아니라면 정렬 (문자열일때는 아스키코드 반환 후 정렬)
 // 3. 변수 i 선언 중복이 아니라면 i++ , 입력 받음 값의 배열의 i 인덱스에 비교 값 할당
 //    전부 순회가 끝나면 중복 있다면 return true 없으면 retrun false
 
@@ -34,7 +34,7 @@ const areThereDuplicates = (...vals) => {
     if (vals[i] !== vals[k]) {
       vals[i] = vals[k];
     } else {
-      return true
+      return true;
     }
   }
 
@@ -44,3 +44,5 @@ const areThereDuplicates = (...vals) => {
 console.log(areThereDuplicates(1, 2, 3));
 console.log(areThereDuplicates(1, 2, 2, 0));
 console.log(areThereDuplicates('a', 'b', 'c', 'a'));
+
+//해설집에는 스트링 문자에 대한 부분이 빠져 있어 제대로 된 값을 가져오지 않는다, 그냥..내가 한 풀이대로 하기로...!
